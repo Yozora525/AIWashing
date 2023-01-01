970a6674f8c7532ac36acc0ff15695ba3c282609
chuhsun <chuhsunkuo@gmail.com>
2023-01-01 15:54:39 +0800

    0101後端測試

--- ChooseWashModetest.php
+++ ChooseWashModetest.php
@@ -3,8 +3,6 @@
 <html lang="en">
 
 <head>
-  <?php //include('templates/frame/head.html') 
-  ?>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
