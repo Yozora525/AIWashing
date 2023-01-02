0ed2ef238c9dc2ecd567ea87a39c81ef04b413f5
SHI-Yan LEE <69109740+Yozora525@users.noreply.github.com>
2023-01-02 10:28:03 +0800

    20230102--shiyan

---
+++ src/orderManage.php
@@ -0,0 +1,13 @@
+<?php
+    // write order data to database
+    function SetOrderData(){
+        // if orderId -> update else insert
+    }
+
+    // get order data from database
+    function GetOrderData(){
+
+    }
+
+
+?>
